6b709c55995258c0ba9309944e3db4b00adc2e28
Shawon5822 <92940025+Shawon5822@users.noreply.github.com>
2025-04-19 23:39:56 +0600

    commit9

---
+++ User5,Goal8/src/main/java/oop_project/u5goal8/HelloController.java
@@ -0,0 +1,14 @@
+package oop_project.u5goal8;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
